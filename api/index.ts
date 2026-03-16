204d91641567cb3d831486d7c2f6ae252e252602
Pham Phu Loc <phamphuloc7663@gmail.com>
2026-03-16 07:20:11 +0700

    push

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,2 @@
-// Use runtime require so Vercel typecheck does not expect dist at compile time.
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const handler = require('../dist/main')
-
+import handler from '../dist/main'
 export default handler
